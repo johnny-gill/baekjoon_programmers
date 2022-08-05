008a364f020df3161f2cbde10f53db67b21a9df5
히히 <55935478+johnny-gill@users.noreply.github.com>
2022-08-05 15:26:57 +0900

    [Silver II] Title: DFS와 BFS, Time: 260 ms, Memory: 20516 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -3,14 +3,16 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
-
 public class Main {
 	
 	static int N, M, V;
-	static boolean[][] node;
+	static List<List<Integer>> node;
 	static boolean[] visit;
 	static BufferedReader br;
 	static BufferedWriter bw;
@@ -24,14 +26,25 @@ public class Main {
 		M = Integer.parseInt(st.nextToken());
 		V = Integer.parseInt(st.nextToken());
 		
-		node = new boolean[N][N];
+		node = new ArrayList<>();
 		visit = new boolean[N];
+		
+		for (int i = 0; i < N; i++) {
+			node.add(new ArrayList<Integer>());
+		}
 		
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			node[a][b] = node[b][a] = true;
+			
+			node.get(a).add(b);
+			node.get(b).add(a);
+		}
+		
+		
+		for (int i = 0; i < N; i++) {
+			Collections.sort(node.get(i));
 		}
 		
 		dfs(V);
@@ -44,32 +57,31 @@ public class Main {
 		br.close();
 	}
 
-	private static void bfs(int start) throws IOException {
-		Queue<Integer> q = new LinkedList<>();
-		q.offer(start);
-		visit[start] = true;
+	private static void dfs(int v) throws IOException {
+		bw.write(String.valueOf(v) + " ");
+		visit[v] = true;
 		
-		while(!q.isEmpty()) {
-			int val = q.poll();
-			bw.write(String.valueOf(val) + " ");
-			
-			for (int i = 1; i < N; i++) {
-				if (node[val][i] && !visit[i]) {
-					visit[i] = true;
-					q.offer(i);
-				}
+		for (int val : node.get(v)) {
+			if(!visit[val]) {
+				dfs(val);
 			}
 		}
 	}
-
-
-	private static void dfs(int start) throws IOException {
-		bw.write(String.valueOf(start) + " ");
-		visit[start] = true;
+	
+	private static void bfs(int v) throws IOException {
+		Queue<Integer> q = new LinkedList<>();
+		q.offer(v);
+		visit[v] = true;
 		
-		for (int i = 1; i < N; i++) {
-			if (node[start][i] && !visit[i]) {
-				dfs(i);
+		while(!q.isEmpty()) {
+			int poll = q.poll();
+			bw.write(String.valueOf(poll) + " ");
+			
+			for (int val : node.get(poll)) {
+				if(!visit[val]) {
+					visit[val] = true;
+					q.offer(val);
+				}
 			}
 		}
 	}
