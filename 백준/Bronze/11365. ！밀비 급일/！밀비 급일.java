dc260ffb24a9f3473947b8dcb28d729491df3c2b
히히 <55935478+johnny-gill@users.noreply.github.com>
2022-08-26 14:24:54 +0900

    [Bronze IV] Title: !밀비 급일, Time: 144 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/11365. ！밀비 급일/！밀비 급일.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	while(true) {
+    		String str = br.readLine();
+    		if ("END".equals(str)) {
+    			break;
+    		}
+    		
+    		
+    		for (int i = str.length() - 1; i >= 0; i--) {
+    			System.out.print(str.charAt(i));
+    		}
+    		
+    		System.out.println();
+    	}
+    }
+}
