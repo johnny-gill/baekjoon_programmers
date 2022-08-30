161b3de18b57b3a00f81c482b02486adce84d8d3
히히 <55935478+johnny-gill@users.noreply.github.com>
2022-08-30 11:39:57 +0900

    [Silver V] Title: 단어 정렬, Time: 568 ms, Memory: 25020 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -2,28 +2,33 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class Main {
-	
-	private static final List<String> words = new ArrayList<>();
 	
 	public static void main(String[] args) throws IOException {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	int line = Integer.parseInt(br.readLine());
-    	while (line-- > 0) {
-    		String str = br.readLine();
-    		if (words.contains(str)) continue;
-    		words.add(str);
+    	String[] words = new String[line];
+    	
+    	for (int i = 0; i < line; i++) {
+    		words[i] = br.readLine();
     	}
 
-    	Collections.sort(words, (o1, o2) -> {
+    	Arrays.sort(words, (o1, o2) -> {
     		int o1Len = o1.length();
 			int o2Len = o2.length();
 			return (o1Len != o2Len) ? o1Len - o2Len : o1.compareTo(o2);
     	});
     	
-    	words.forEach(word -> System.out.println(word));
+    	String prev = "";
+    	for (String word : words) {
+    		if (prev.equals(word)) continue;
+    		prev = word;
+			System.out.println(word);
+		}
     }
 }
