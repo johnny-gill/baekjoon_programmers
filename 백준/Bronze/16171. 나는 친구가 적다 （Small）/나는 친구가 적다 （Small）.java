1245bbdf1bd8cab617614b82eebdfb6bc9f359b6
히히 <55935478+johnny-gill@users.noreply.github.com>
2022-08-30 15:28:44 +0900

    [Bronze I] Title: 나는 친구가 적다 (Small), Time: 124 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Bronze/16171. 나는 친구가 적다 （Small）/나는 친구가 적다 （Small）.java
+++ 백준/Bronze/16171. 나는 친구가 적다 （Small）/나는 친구가 적다 （Small）.java
@@ -8,28 +8,8 @@ public class Main {
 	public static void main(String[] args) throws IOException {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	
-    	String str1 = br.readLine();
+    	String str1 = br.readLine().replaceAll("[0-9]", "");
     	String str2 = br.readLine();
-    	
-    	String newStr1 = "";
-    	for (char c : str1.toCharArray()) {
-			if ('a' <= c && c <= 'z' || 'A' <= c && c <= 'Z') {
-				newStr1 += c;
-			}
-		}
-    	
-    	int str1Len = newStr1.length();
-    	int str2Len = str2.length();
-    	int isExist = 0;
-		for (int i = 0; i < str1Len; i++) {
-			if (str1Len < i + str2Len) break;
-			String subStr = newStr1.substring(i, i + str2Len);
-    		if (str2.equals(subStr)) {
-    			isExist = 1;
-    			break;
-    		}
-    	}
-		
-		System.out.println(isExist);
+    	System.out.println(str1.contains(str2) ? 1 : 0); 
     }
 }
