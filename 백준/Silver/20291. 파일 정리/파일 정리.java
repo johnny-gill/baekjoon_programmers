24ee02b19302b0cae65365c0d6ff71b46aa945dd
히히 <55935478+johnny-gill@users.noreply.github.com>
2022-08-31 14:14:15 +0900

    [Silver III] Title: 파일 정리, Time: 1420 ms, Memory: 61396 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.java
+++ 백준/Silver/20291. 파일 정리/파일 정리.java
@@ -2,17 +2,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class Main {
 	
 	public static void main(String[] args) throws IOException {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	
-    	Map<String, Integer> map = new HashMap<>();
+    	Map<String, Integer> map = new TreeMap<>();
     	int line = Integer.parseInt(br.readLine());
     	while(line-- > 0) {
     		String ext = br.readLine().split("\\.")[1];
@@ -20,7 +19,6 @@ public class Main {
     	}
     	
     	List<String> keyList = new ArrayList<>(map.keySet());
-    	Collections.sort(keyList);
     	
     	for (String key : keyList) {
 			System.out.println(key + " " + map.get(key));
